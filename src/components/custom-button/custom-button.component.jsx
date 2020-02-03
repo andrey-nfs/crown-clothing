f2f404673a4feb60830ce9a207fd8a5170a48b39
Andrey Ignatyev <ignatyev@andrey.com.au>
2020-02-03 20:16:19 +1100

    shop data updated, cart reducer enhanced to count items added

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 import './custom-button.styles.scss';
 
-const CustomButton = ({ children, isGoogleSignin, ...otherProps }) => (
-  <button className={`custom-button${isGoogleSignin ? ' google-sign-in' : ''}`} {...otherProps}>
+const CustomButton = ({ children, isGoogleSignin, inverted, ...otherProps }) => (
+  <button className={`custom-button${inverted ? ' inverted' : ''}${isGoogleSignin ? ' google-sign-in' : ''}`} {...otherProps}>
     {children}
   </button>
 )
