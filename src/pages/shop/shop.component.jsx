779219b7858a37e4edb64fefe4fc63e4fbb860f5
Andrey Ignatyev <andrey@carterdigital.com.au>
2020-04-02 18:29:04 +1100

    Replaced lifecycle methods with useEffect() hooks

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 
@@ -6,22 +6,17 @@ import CollectionPageContainer from '../../pages/collection/collection.container
 import CollectionsOverviewContainer from '../../components/collections-overview/collections-overview.container';
 import { fetchCollectionsStart } from '../../redux/shop/shop.actions';
 
-class ShopPage extends React.Component {
-  componentDidMount() {
-    const { fetchCollectionsStart } = this.props;
+const ShopPage = ({ fetchCollectionsStart, match }) => {
+  useEffect(() => {
     fetchCollectionsStart();
-  }
+  }, [fetchCollectionsStart]);
 
-  render() {
-    const { match } = this.props;
-
-    return(
-      <div>
-        <Route exact path={`${match.path}`} component={CollectionsOverviewContainer} />
-        <Route path={`${match.path}/:collectionId`} component={CollectionPageContainer} />
-      </div>
-    )
-  }
+  return (
+    <div>
+      <Route exact path={`${match.path}`} component={CollectionsOverviewContainer} />
+      <Route path={`${match.path}/:collectionId`} component={CollectionPageContainer} />
+    </div>
+  )
 }
 
 const mapDispatchToProps = dispatch => ({
